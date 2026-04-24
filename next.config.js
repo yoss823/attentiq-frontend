0b095b4b59b2124a6f8d6bbf999bdccdf0c84d99
yoss823 <yosseffarahat1992@gmail.com>
2026-04-24 16:25:27 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://attentiqbackend-production.up.railway.app',
   },
